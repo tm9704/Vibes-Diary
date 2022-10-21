1ff86e27f9ba2a9fd761073848242c873cbb7798
Min Hyung Lee <tm9704@korea.ac.kr>
2022-10-21 17:16:30 +0900

    Vibes Diary : Routing Apply

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
@@ -17,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
         <RounteTest />
       </div>
