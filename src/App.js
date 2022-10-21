e874ba9631b70e33d75fad70ccac8ce96bfe70ac
Min Hyung Lee <tm9704@korea.ac.kr>
2022-10-21 16:57:53 +0900

    Vibe Diary: Routing Basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import RouteTest from "./components/RouteTest";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import RounteTest from "./components/RouteTest";
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RounteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
